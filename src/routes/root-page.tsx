89b2a4403042c37f1ad97cf0caf05635dc443b0a
German Levental <gerleven@gmail.com>
2023-11-19 00:30:37 -0300

    Css class to implement mobile-first

--- src/routes/root-page.tsx
+++ src/routes/root-page.tsx
@@ -1,10 +1,20 @@
 import { Outlet } from "react-router-dom";
+import "../App.css"
 
 export default function Root() {
   return (
     <>
-      <h1>Root</h1>
-      <Outlet />
+    <div className="fullscreen">
+      <div className="app-wrapper">
+        <h1>Root</h1>
+        <div className="app">
+          <h3>Mercado Pago</h3>
+        </div>
+        <div className="app-content">
+          <Outlet />
+        </div>
+      </div>
+    </div>
     </>
   );
 }
