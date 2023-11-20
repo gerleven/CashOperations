00edb08b66c68551ccfe96f94e23f2d396e45d0c
German Levental <gerleven@gmail.com>
2023-11-20 19:53:41 -0300

    add a new 'brandColors' variable to the theme

--- src/main.tsx
+++ src/main.tsx
@@ -14,12 +14,48 @@ import ErrorPage from "./routes/error-page.tsx";
 
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
+//You have to use module augmentation to add new variables to the Theme and ThemeOptions.
+declare module '@mui/material/styles' {
+  interface Theme {
+    brandColors?:{
+      "blueBanner": string,
+      "primary": string,
+      "secondary": string,
+      "lightGrey": string,
+      "white": string,
+      "darkGrey": string,
+      "black": string,
+    }
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    brandColors?:{
+      "blueBanner": string,
+      "primary": string,
+      "secondary": string,
+      "lightGrey": string,
+      "white": string,
+      "darkGrey": string,
+      "black": string,
+    }
+  }
+}
+
 const theme = createTheme({
   palette: {
     primary: { main: "#3483FA", light: "#009EE3" },
     secondary: { main: "rgba(65, 137, 230, 0.15);" },
     common: { black: "rgba(0, 0, 0, 0.90);", white: "#FFFFFF" },
     grey: { 200: "#EDEDED", 600: "rgba(0, 0, 0, 0.55);" },
+  },
+  brandColors:{
+    blueBanner: "#009ee3",
+    primary: "#3483fa",
+    secondary: "rgba(65, 137, 230, 0.15)",
+    lightGrey: "#ededed",
+    white: "#ffffff",
+    darkGrey: "rgba(0, 0, 0, 0.55)",
+    black: "rgba(0, 0, 0, 0.9)",
   },
   components: {
     MuiIconButton: {
