fa8814823b0cfb8c248ee3c14e28ced4adffa05c
German Levental <gerleven@gmail.com>
2023-11-21 00:04:19 -0300

    ghost changues, prettier

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -5,11 +5,11 @@ import IconButton from "@mui/material/IconButton";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 export default function Banner() {
-      return (
+  return (
     <>
-          <div className="banner">
-        <IconButton sx={{marginLeft: "16px"}}>
-          <ArrowBackIcon/>
+      <div className="banner">
+        <IconButton sx={{ marginLeft: "16px" }}>
+          <ArrowBackIcon />
         </IconButton>
         <img src={logo} alt="Mercado_Pago_imagen" className="banner-logo" />
       </div>
--- src/helpers/CustomTheme.tsx
+++ src/helpers/CustomTheme.tsx
@@ -1,79 +1,77 @@
 import { createTheme } from "@mui/material/styles";
 
 //You have to use module augmentation to add new variables to the Theme and ThemeOptions.
-declare module '@mui/material/styles' {
-    interface Theme {
-      brandColors?:{
-        "blueBanner": string,
-        "primary": string,
-        "secondary": string,
-        "lightGrey": string,
-        "white": string,
-        "darkGrey": string,
-        "black": string,
-      }
-    }
-    // allow configuration using `createTheme`
-    interface ThemeOptions {
-      brandColors?:{
-        "blueBanner": string,
-        "primary": string,
-        "secondary": string,
-        "lightGrey": string,
-        "white": string,
-        "darkGrey": string,
-        "black": string,
-      }
-    }
+declare module "@mui/material/styles" {
+  interface Theme {
+    brandColors?: {
+      blueBanner: string;
+      primary: string;
+      secondary: string;
+      lightGrey: string;
+      white: string;
+      darkGrey: string;
+      black: string;
+    };
   }
-  
-  const theme = createTheme({
-    palette: {
-      primary: { main: "#3483FA", light: "#009EE3" },
-      secondary: { main: "rgba(65, 137, 230, 0.15);" },
-      common: { black: "rgba(0, 0, 0, 0.90);", white: "#FFFFFF" },
-      grey: { 200: "#EDEDED", 600: "rgba(0, 0, 0, 0.55);" },
-    },
-    brandColors:{
-      blueBanner: "#009ee3",
-      primary: "#3483fa",
-      secondary: "rgba(65, 137, 230, 0.15)",
-      lightGrey: "#ededed",
-      white: "#ffffff",
-      darkGrey: "rgba(0, 0, 0, 0.55)",
-      black: "rgba(0, 0, 0, 0.9)",
-    },
-    components: {
-      MuiIconButton: {
-        defaultProps:{
-          style: {padding: "0"}
-        },
-        styleOverrides: {
-        }
-      },
-      MuiButtonBase: {
-        defaultProps: {
-          disableRipple: true,
-        },
-        styleOverrides: {
-        }
-      },
-      MuiSvgIcon: {
-        defaultProps:{},
-        styleOverrides: {
-          root: ({ theme }) =>
-            theme.unstable_sx({
-              height: "32px",
-              width: "32px",
-              color: theme.palette.common.white
-            })
-        }
-      }
-    },
-    typography: {
-      fontFamily: "Roboto, Helvetica, Arial, sans-serif",
-      fontSize: 14,
-    },
-  });
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    brandColors?: {
+      blueBanner: string;
+      primary: string;
+      secondary: string;
+      lightGrey: string;
+      white: string;
+      darkGrey: string;
+      black: string;
+    };
+  }
+}
 
-  export default theme;
+const theme = createTheme({
+  palette: {
+    primary: { main: "#3483FA", light: "#009EE3" },
+    secondary: { main: "rgba(65, 137, 230, 0.15);" },
+    common: { black: "rgba(0, 0, 0, 0.90);", white: "#FFFFFF" },
+    grey: { 200: "#EDEDED", 600: "rgba(0, 0, 0, 0.55);" },
+  },
+  brandColors: {
+    blueBanner: "#009ee3",
+    primary: "#3483fa",
+    secondary: "rgba(65, 137, 230, 0.15)",
+    lightGrey: "#ededed",
+    white: "#ffffff",
+    darkGrey: "rgba(0, 0, 0, 0.55)",
+    black: "rgba(0, 0, 0, 0.9)",
+  },
+  components: {
+    MuiIconButton: {
+      defaultProps: {
+        style: { padding: "0" },
+      },
+      styleOverrides: {},
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+      },
+      styleOverrides: {},
+    },
+    MuiSvgIcon: {
+      defaultProps: {},
+      styleOverrides: {
+        root: ({ theme }) =>
+          theme.unstable_sx({
+            height: "32px",
+            width: "32px",
+            color: theme.palette.common.white,
+          }),
+      },
+    },
+  },
+  typography: {
+    fontFamily: "Roboto, Helvetica, Arial, sans-serif",
+    fontSize: 14,
+  },
+});
+
+export default theme;
