4085cf5acd3cf0216b1757594ccdfaac165264eb
German Levental <gerleven@gmail.com>
2023-11-22 13:00:25 -0300

    add Aplicar and Limpiar buttons on Select

--- src/utils/custom-select.tsx
+++ src/utils/custom-select.tsx
@@ -4,25 +4,23 @@ import FormControl from "@mui/material/FormControl";
 import ListItemText from "@mui/material/ListItemText";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import Checkbox from "@mui/material/Checkbox";
+import { CustomButtonPrimary, CustomButtonSecondary } from "./custom-buttons";
+import { ListSubheader, Stack } from "@mui/material";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
 const MenuProps = {
   PaperProps: {
     style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      maxHeight: ITEM_HEIGHT * 5.5 + ITEM_PADDING_TOP,
       width: 250,
     },
   },
 };
 
-const names = [
-  "Cobros con QR",
-  "Cobros con tarjeta",
-  "Cobros en efectivo",
-];
+const names = ["Cobros con QR", "Cobros con tarjeta", "Cobros en efectivo"];
 
-export function CustomSelect({filters, handleChange}: any) {
+export function CustomSelect({ filters, handleChange, handleClickApply, handleClickClean }: any) {
   return (
     <>
       {/* <InputLabel id="demo-multiple-checkbox-label">{`Filtros (${filters.length})`}</InputLabel> */}
@@ -33,15 +31,27 @@ export function CustomSelect({filters, handleChange}: any) {
         value={filters}
         onChange={handleChange}
         // input={<OutlinedInput label={`Filtros (${filters.length})`} />}
-        renderValue={() => `Filtros (${filters.length-1})`}
+        renderValue={() => `Filtros (${filters.length - 1})`}
         MenuProps={MenuProps}
       >
         {names.map((filter) => (
           <MenuItem key={filter} value={filter}>
-            <Checkbox checked={filters.indexOf(filter) > -1} color="primary"/>
+            <Checkbox checked={filters.indexOf(filter) > -1} color="primary" />
             <ListItemText primary={filter} />
           </MenuItem>
         ))}
+        <ListSubheader>
+          <Stack
+            direction={"row"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            gap={1}
+            sx={{width: "100%", padding: "10px"}}
+          >
+            <CustomButtonPrimary onClick={handleClickApply}>Aplicar</CustomButtonPrimary>
+            <CustomButtonSecondary onClick={handleClickClean}>Limpiar</CustomButtonSecondary>
+          </Stack>
+        </ListSubheader>
       </Select>
     </>
   );
@@ -59,11 +69,23 @@ export default function MultipleSelectCheckmarks() {
       typeof value === "string" ? value.split(",") : value
     );
   };
+  const handleClickApply=()=>{
+    alert("handleClickApply");
+  }
+
+  const handleClickClean=()=>{
+    alert("handleClickClean");
+  }
 
   return (
     <div>
       <FormControl fullWidth>
-        <CustomSelect filters={filters} handleChange={handleChange}></CustomSelect>
+        <CustomSelect
+          filters={filters}
+          handleChange={handleChange}
+          handleClickApply={handleClickApply}
+          handleClickClean={handleClickClean}
+        ></CustomSelect>
       </FormControl>
     </div>
   );
